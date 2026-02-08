0d85b2781d47132125bfefedc21fad0a1f21aeac
axeboyrocks <rocksunnychauhan@gmail.com>
2026-02-08 17:11:25 -0500

    Initial invoicing PWA

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#ffffff" />
+      <link rel="apple-touch-icon" href="/icon-192.png" />
+    </>
+  );
+}
